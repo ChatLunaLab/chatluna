d5374e90c8f16def6745a04c2a457f86a6752770
dingyi <dingyi222666@foxmail.com>
2023-08-27 02:13:21 +0800

    refactor(test-adapter): use new platform api

--- packages/test-adapter/src/index.ts
+++ packages/test-adapter/src/index.ts
@@ -1,20 +1,74 @@
-import { ModelProvider, CreateParams, BaseProvider, ChatHubBaseChatModel } from '@dingyi222666/koishi-plugin-chathub/lib/llm-core/model/base'
-import { PromiseLikeDisposeable } from '@dingyi222666/koishi-plugin-chathub/lib/utils/types'
 import { ChatHubPlugin } from "@dingyi222666/koishi-plugin-chathub/lib/services/chat"
 import { Context, Schema } from 'koishi'
-import { BaseChatModel } from 'langchain/chat_models/base'
-import { CallbackManagerForLLMRun, Callbacks } from 'langchain/callbacks'
-import { BaseMessage, ChatResult, ChatGeneration, AIMessage } from 'langchain/schema'
+import { ClientConfig } from '@dingyi222666/koishi-plugin-chathub/lib/llm-core/platform/config'
+import { PlatformModelClient } from '@dingyi222666/koishi-plugin-chathub/lib/llm-core/platform/client'
+import { ChatHubChatModel } from '@dingyi222666/koishi-plugin-chathub/lib/llm-core/platform/model'
+import { ModelInfo, ModelType } from '@dingyi222666/koishi-plugin-chathub/lib/llm-core/platform/types'
+import { ModelRequestParams, ModelRequester } from '@dingyi222666/koishi-plugin-chathub/lib/llm-core/platform/api'
+import { AIMessageChunk, ChatGenerationChunk } from 'langchain/dist/schema'
 
-class TestPlugin extends ChatHubPlugin<TestPlugin.Config> {
-    name = "@dingyi222666/chathub-test-adapter"
 
-    public constructor(protected ctx: Context, public readonly config: TestPlugin.Config) {
-        super(ctx, config)
+export function apply(ctx: Context, config: Config) {
+    const plugin = new ChatHubPlugin<ClientConfig, Config>(ctx, config, "test")
 
-        setTimeout(async () => {
-            await ctx.chathub.registerPlugin(this)
-            this.registerModelProvider(new TestModelProvider(config))
+    ctx.on("ready", async () => {
+
+        await plugin.parseConfig(() => [{
+            apiKey: "?",
+            platform: "test",
+            maxRetries: config.maxRetries,
+            chatLimit: config.chatTimeLimit,
+            concurrentMaxSize: config.chatConcurrentMaxSize
+        }])
+
+        await plugin.registerClient((ctx, config) => {
+            return new TestPlatformClient(ctx, config)
+        })
+
+        await plugin.registerToService()
+    })
+}
+
+
+class TestPlatformClient extends PlatformModelClient {
+
+    platform: string = "test"
+
+    async init(): Promise<void> {}
+
+    async getModels(): Promise<ModelInfo[]> {
+        return [{
+            name: "test",
+            type: ModelType.llm
+        }]
+    }
+
+    protected _createModel(model: string): ChatHubChatModel {
+        return new ChatHubChatModel({
+            model: model,
+            modelMaxContextSize: 10000000000,
+            requester: new TestModelRequester()
+        })
+    }
+
+}
+
+
+class TestModelRequester extends ModelRequester {
+
+    async *completionStream(params: ModelRequestParams): AsyncGenerator<ChatGenerationChunk> {
+
+        const messages = params.input
+
+        console.log(`messages: ${JSON.stringify(messages)}`)
+
+        const latestMessage = messages[messages.length - 1]
+
+        const response = latestMessage.content.replaceAll("你", "我").replaceAll('?', '!').replaceAll("不", " ").replaceAll("吗", " ").replaceAll("有", "没有").replaceAll('？', '！')
+
+        yield new ChatGenerationChunk({
+            text: response,
+            message: new AIMessageChunk(response)
         })
     }
 
@@ -22,81 +76,13 @@ class TestPlugin extends ChatHubPlugin<TestPlugin.Config> {
 
 
 
+export interface Config extends ChatHubPlugin.Config { }
 
-class TestModelProvider extends ModelProvider {
-
-    constructor(private readonly config: TestPlugin.Config) {
-        super()
-    }
-
-
-    private _models = ['test']
-
-    async createModel(modelName: string, params: CreateParams): Promise<ChatHubBaseChatModel> {
-        return new TestChatModel(params)
-    }
-    listModels(): Promise<string[]> {
-        return Promise.resolve(this._models)
-    }
-    name = "testProvider"
-    description?: string;
-    isSupported(modelName: string): Promise<boolean> {
-        return Promise.resolve(this._models.includes(modelName))
-    }
+export const Config = Schema.intersect([
+    ChatHubPlugin.Config,
+])
 
 
-    getExtraInfo(): Record<string, any> {
-        return this.config
-    }
-}
+export const using = ['chathub']
 
-
-class TestChatModel extends ChatHubBaseChatModel {
-
-    _llmType() {
-        return "test";
-    }
-
-    /** @ignore */
-    _combineLLMOutput() {
-        return [];
-    }
-
-    async _generate(
-        messages: BaseMessage[],
-        options: this["ParsedCallOptions"],
-        callbacks?: CallbackManagerForLLMRun): Promise<ChatResult> {
-
-        console.log(`messages: ${JSON.stringify(messages)}`)
-
-        const lastestMessage = messages[messages.length - 1]
-
-        const generations: ChatGeneration[] = [];
-
-        const response = lastestMessage.content.replaceAll("你", "我").replaceAll('?', '!').replaceAll("不", " ").replaceAll("吗", " ").replaceAll("有", "没有").replaceAll('？', '！')
-
-        generations.push({
-            text: response,
-            message: new AIMessage(response)
-        });
-
-        return {
-            generations
-
-        }
-    }
-}
-
-
-namespace TestPlugin {
-    export interface Config extends ChatHubPlugin.Config { }
-
-    export const Config = Schema.intersect([
-        ChatHubPlugin.Config,
-    ])
-
-
-    export const using = ['chathub']
-}
-
-export default TestPlugin
+export const name = "@dingyi222666/chathub-test-adapter"
