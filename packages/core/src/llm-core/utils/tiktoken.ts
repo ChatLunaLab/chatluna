c4eaed54623dd68339c010b3b5dbbe6b1a3966f4
dingyi <dingyi222666@foxmail.com>
2024-10-14 15:58:43 +0800

    fix(core): use CDN for tiktoken and add error handling
    - Replace direct dependency on js-tiktoken with a CDN version
    - Add error handling and retry mechanism for fetching tiktoken encodings
    - Implement timeout and signal support for fetch requests
    - Update package.json to reflect new dependency source

--- packages/core/src/llm-core/utils/tiktoken.ts
+++ packages/core/src/llm-core/utils/tiktoken.ts
@@ -1,23 +1,68 @@
 import {
-    getEncoding,
     getEncodingNameForModel,
     Tiktoken,
+    TiktokenBPE,
+    TiktokenEncoding,
     TiktokenModel
-} from 'js-tiktoken'
+} from 'js-tiktoken/lite'
+import { chatLunaFetch } from 'koishi-plugin-chatluna/utils/request'
 
-const cache: Record<string, Tiktoken> = {}
+const cache: Record<string, TiktokenBPE> = {}
+
+export let errorCount = 0
+
+export async function getEncoding(
+    encoding: TiktokenEncoding,
+    options?: {
+        signal?: AbortSignal
+        extendedSpecialTokens?: Record<string, number>
+    }
+) {
+    if (errorCount > 3) {
+        throw new Error('Too many errors')
+    }
+    if (!(encoding in cache)) {
+        cache[encoding] = await chatLunaFetch(
+            `https://tiktoken.pages.dev/js/${encoding}.json`,
+            {
+                signal: options?.signal
+            }
+        )
+            .then((res) => res.json() as unknown as TiktokenBPE)
+            .catch((e) => {
+                errorCount++
+
+                delete cache[encoding]
+                throw e
+            })
+    }
+
+    return new Tiktoken(cache[encoding], options?.extendedSpecialTokens)
+}
 
 export async function encodingForModel(
     model: TiktokenModel,
     options?: {
+        signal?: AbortSignal
         extendedSpecialTokens?: Record<string, number>
     }
 ) {
-    const encoding = getEncodingNameForModel(model)
+    options = options ?? {}
 
-    if (!(encoding in cache)) {
-        cache[encoding] = getEncoding(encoding, options?.extendedSpecialTokens)
+    let timeout: NodeJS.Timeout
+
+    if (options.signal == null) {
+        const abortController = new AbortController()
+
+        options.signal = abortController.signal
+
+        timeout = setTimeout(() => abortController.abort(), 1000 * 25)
+    }
+    const result = await getEncoding(getEncodingNameForModel(model), options)
+
+    if (timeout != null) {
+        clearTimeout(timeout)
     }
 
-    return cache[encoding]
+    return result
 }
