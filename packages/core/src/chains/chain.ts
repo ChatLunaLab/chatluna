e553728b6742a650880d03a6b9e938ffdee57dd0
dingyi <dingyi222666@foxmail.com>
2026-03-07 20:22:58 +0800

    fix(core): add optional chaining for message type check in QQ markdown handler

--- packages/core/src/chains/chain.ts
+++ packages/core/src/chains/chain.ts
@@ -725,7 +725,7 @@ class DefaultChatChainSender {
 
         if (
             isElementArray(messages?.[0]) &&
-            messages[0][1].type === 'markdown-qq'
+            messages[0][1]?.type === 'markdown-qq'
         ) {
             await this.sendAsQQMarkdown(session, messages[0][0])
             return
